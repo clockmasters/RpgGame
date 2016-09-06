429af07a540e68fd99b312a39bca50a3831c7564
douglas <douglas@douglas-Inspiron-3442>
2016-09-05 23:33:08 -0300

    Primeiro Commit - Criacao Classe Launcher

---
+++ src/dev/rpggame/Launcher.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dev.rpggame;
+
+import dev.rpggame.display.Display;
+
+/**
+ *
+ * @author douglas
+ * @version 1-05/09/2016
+ * @languageOfComments: Portuguese
+ */
+public class Launcher {
+    public static void main(String[] args){
+       Display d = new Display("ClockMasters!", 300, 300);
+    }
+}
